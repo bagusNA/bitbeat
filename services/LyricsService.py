7f23fdd6eba1199a5c9faac8070208fd58421578
Bagus Nur Andiansyah <bagusnur@protonmail.com>
2023-11-29 17:27:56 +0800

    Misc: filter lyrics a lil bit

--- services/LyricsService.py
+++ services/LyricsService.py
@@ -40,13 +40,21 @@ class LyricsService(QObject):
         self._title = self._service.audio_player.current_song.title
         self._worker.start()
 
+    def format_lyrics(self, lyrics: str):
+        lines = lyrics.splitlines()
+        lines.pop(0)  # Removes unnecessary contributor line
+        lines.pop(0)  # Removes empty line
+
+        filtered_lines = filter(lambda line: not line.startswith('[') and not line.endswith(']'), lines)
+        return filtered_lines
+
+
     @Slot(object)
     def on_lyrics_fetched(self, lyrics: str | None):
         if lyrics is None:
             self.lyricsFetched.emit(None)
             return
 
-        lyric_lines = lyrics.splitlines()
-        lyric_lines.pop(0)  # Removes unnecessary contributor line
+        lyric_lines = self.format_lyrics(lyrics)
 
         self.lyricsFetched.emit(lyric_lines)
